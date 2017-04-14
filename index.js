e1b717c18144917f973d5f91d3deb4852ed86a84
TheNesze <zsolt@csaky.hu>
2017-04-14 16:42:33 +0200

    0.0.2

--- index.js
+++ index.js
@@ -2,44 +2,80 @@ const fs = require('fs')
 const Discord = require('discord.js')
 
 var token = fs.readFileSync('./token.txt').toString()
+var cfg = JSON.parse(fs.readFileSync('config.json').toString())
+var strings = JSON.parse(fs.readFileSync('strings.json').toString())
 
 const client = new Discord.Client()
 
 client.on('ready', () => {
-    console.log('I am ready!')
+    console.log(strings.inf.ready)
 });
 
 client.on('message', message => {
-    if (message.content.indexOf('ping') >= 0) {
-        message.channel.sendMessage('pong')
-    } else if (message.content == 'ájjále') {
-        if (permitted(message.author.username)) {
-            message.channel.sendMessage('baibai').then(() => {
-                client.destroy()
-            }); 
-        } else {
-            message.channel.sendMessage('Nincs jogosultságod a parancs használatához.')
-        }
-    } else if (message.content.startsWith('pucolj')) {
-        var number = parseInt(message.content.split(' ')[1])
-        if (number > 10) {
-            message.channel.sendMessage('Max 10 üzenetet törölhetsz egyszerre.')
-        } else {
-            message.channel.bulkDelete(number + 1)
+    if (message.content.startsWith(cfg.prefix)) {
+        var msg = message.content.substr(1).split(' ')
+        switch (msg[0]) {
+            case 'set':
+                switch (msg[1]) {
+                    case 'prefix':
+                        cfg.prefix = msg[2]
+                        saveConfig()
+                        message.channel.sendMessage(strings.inf.commandPrefixChanged + cfg.prefix)
+                        break
+                    default:
+                        message.channel.sendMessage(strings.err.varNotFound + msg[1])
+                }
+                break
+            case 'get':
+                switch (msg[1]) {
+                    case 'prefix':
+                        message.channel.sendMessage(strings.inf.commandPrefix + cfg.prefix)
+                        break
+                    default:
+                        message.channel.sendMessage(strings.err.varNotFound + msg[1])
+                }
+                break
+            case 'delete':
+                var n = parseInt(msg[1])
+                if (n > 10) {
+                    message.channel.sendMessage(strings.inf.maxDeleteCount)
+                } else {
+                    message.channel.bulkDelete(n + 1).then(() => {
+                        message.channel.sendMessage(n + strings.inf.deleteSuccessful).then((tempMsg) => {
+                            setTimeout(() => {
+                                tempMsg.delete()
+                            }, 3000)
+                        })
+                    })
+                }
+                break
+            case 'stop':
+                message.channel.sendMessage(strings.inf.shutdown).then(() => {
+                    client.destroy()
+                })
+                break
+            case 'reload':
+                switch(msg[1]){
+                    case 'strings':
+                        updateStrings()
+                        message.channel.sendMessage(strings.inf.messagesUpdated)
+                        break
+                    default:
+                        message.channel.sendMessage(strings.err.varNotFound+msg[1])
+                }
+                break
+            default:
+                message.channel.sendMessage(strings.err.commandNotFound)
         }
     }
-});
-
+})
 
 client.login(token)
 
+function saveConfig() {
+    fs.writeFileSync('config.json', JSON.stringify(cfg))
+}
 
-var usernamelist = ['Nesze', 'legekkaka']
-
-function permitted(username) {
-    if (usernamelist.indexOf(username) >= 0) {
-        return true
-    } else {
-        return false
-    }
+function updateStrings(){
+    strings = JSON.parse(fs.readFileSync('strings.json').toString())
 }
